6793987ba4560b7f66340ee8356a23d803ea6c11
ddpp9199-arch <ddpp9199@gmail.com>
2026-05-12 16:18:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const http = require('http');
+// สร้าง Server หลอกๆ เพื่อให้ Render เลิกบ่นเรื่อง Port
+http.createServer((req, res) => {
+   res.write("Z-Sora is Online!");
+   res.end();
+}).listen(process.env.PORT || 8080);
+
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({
     intents: [
@@ -8,7 +15,7 @@ const client = new Client({
 });
 
 client.once('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`เข้าสู่ระบบโดยใช้ชื่อผู้ใช้ ${client.user.tag}!`);
 });
 
 client.on('messageCreate', message => {
